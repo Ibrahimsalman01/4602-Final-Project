406412619df09a39aa7b4734fb50656de935f932
Ibrahim <ibrahimsalman01@gmail.com>
2026-03-19 12:08:03 -0400

    Function to discover movies based on dates

--- .venv/main.py
+++ .venv/main.py
@@ -1,6 +1,7 @@
 import tmdbsimple as tmdb
 
 from dotenv import load_dotenv
+import pprint
 import os
 
 load_dotenv()
@@ -9,10 +10,25 @@ load_dotenv()
 tmdb.API_KEY = os.getenv('TMDB_API_KEY')
 tmdb.REQUESTS_TIMEOUT = 5
 
-# grabbing inception
-# CHECK /find IN THE TMDB DOCS
-search = tmdb.Search()
-response = search.movie(query='The Bourne')
 
-for s in search.results:
-    print(s['title'], s['id'], s['release_date'], s['popularity'])
+def popular_movies_by_date(start_date, end_date):
+    """
+    Date format: 'YYYY-MM-DD'
+
+    Example (Year of 2016): popular_movies_by_date('2016-01-01', '2016-12-31')
+    """
+    discover = tmdb.Discover()
+    discover_kwargs = {
+        'primary_release_date.gte': start_date,
+        'primary_release_date.lte': end_date,
+        'sort_by': 'popularity.desc'
+    }
+
+    return discover.movie(**discover_kwargs)
+
+# Testing for year of 2025
+response1 = popular_movies_by_date('2025-01-01', '2025-12-31')
+pprint.pp(response1['results'][:3])
+# For some reason Avatar: Fire and Ash doesn't appear on this list or,
+# even in the documentations testing environment: https://developer.themoviedb.org/reference/discover-movie
+# Still need to discuss what result we're expecting so we know what to filter by, but this is a start for gathering data
